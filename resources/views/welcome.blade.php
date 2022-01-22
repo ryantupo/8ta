cb92fb6fd6951c21d45d4bb03f04399011746a06
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-01-22 21:50:34 +0000

    additon of navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,13 @@
 
 <head>
     <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>8ta</title>
+
+    @include('layouts.header')
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -414,35 +418,18 @@
 
     </style>
 
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-
-    </style>
 </head>
 
 <body class="antialiased">
-    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-        <img src="{{ asset('images/8taLogo.png') }}" class="w-30 p-6" alt="8talogo" title="" />
-        <a href="/blog">Blog Posts</a>
-    </div>
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
+
+    <div id="main" class="row">
+
+        @yield('content')
+
+    </div>
+
+    <div class="">
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
