df7fc930dcfb52645f84533ea97c83ad328e0e6b
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-01-22 21:50:12 +0000

    implementation of navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,3 +21,19 @@
         @endif
     </div>
 </body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto" style="margin-left: 1.5rem">
+        <li class="nav-item active">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/blog">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
