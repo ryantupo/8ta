213bb870af63f74178baa56e98465b0982ea6ab4
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-02-11 23:33:54 +0000

    new error pages

--- resources/views/charts/testChart.blade.php
+++ resources/views/charts/testChart.blade.php
@@ -14,7 +14,7 @@
             <h1 class="mx-auto pt-5" style="width: 200px;">Test Chart</h1>
         </div>
 
-        <div>
+        <div style="width: 60%; margin:auto;">
             <canvas id="myChart"></canvas>
         </div>
 
@@ -26,10 +26,7 @@
         <script>
             var elem = document.getElementById('chart');
             var config = JSON.parse(elem.getAttribute('data-type'));
-
         </script>
-
-
 
         <script>
             const myChart = new Chart(
@@ -41,3 +38,53 @@
     </body>
 
 @endsection
+
+{{--
+const DATA_COUNT = 5;
+const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+const data = {
+  labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: Utils.numbers({
+          "count": 5,
+          "min": 0,
+          "max": 100
+    }),
+      backgroundColor: Object.values(Utils.CHART_COLORS),
+    }
+  ]
+};
+
+
+const config = {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: Utils.numbers({
+                "count": 5,
+                "min": 0,
+                "max": 100
+          }),
+            backgroundColor: Object.values(Utils.CHART_COLORS),
+          }
+        ]
+      },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Doughnut Chart'
+        }
+      }
+    },
+  };  --}}
