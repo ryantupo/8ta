f8cef27e6d9f8f79d86ee05145777acb9d7a4809
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-02-20 18:54:11 +0000

    resize charts

--- resources/views/charts/testChart.blade.php
+++ resources/views/charts/testChart.blade.php
@@ -14,28 +14,25 @@
             <h1 class="mx-auto pt-5" style="width: 200px;">Test Chart</h1>
         </div>
 
-        <div style="width: 60%; margin:auto;">
+        <div style="width: 60%; height:60%; margin:auto;">
             <canvas id="myChart"></canvas>
         </div>
 
-        <div id="chart" data-type="{{$chart->config}}"><div>
+        <div id="chart" data-type="{{ $chart->config }}">
+            <div>
 
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script>
+                    var elem = document.getElementById('chart');
+                    var config = JSON.parse(elem.getAttribute('data-type'));
+                </script>
 
-
-        <script>
-            var elem = document.getElementById('chart');
-            var config = JSON.parse(elem.getAttribute('data-type'));
-        </script>
-
-        <script>
-            const myChart = new Chart(
-                document.getElementById('myChart'),
-                config
-            );
-        </script>
+                <script>
+                    const myChart = new Chart(
+                        document.getElementById('myChart'),
+                        config
+                    );
+                </script>
 
     </body>
-
 @endsection
-
