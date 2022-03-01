db29ae56be6b316ebeca97c4fca6ee9fde767032
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-03-01 22:38:43 +0000

    store function added to insert charts

--- app/Http/Controllers/chartController.php
+++ app/Http/Controllers/chartController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chart;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use \Colors\RandomColor;
 
 class chartController extends Controller
 {
@@ -14,7 +17,7 @@ class chartController extends Controller
      */
     public function index()
     {
-        //
+        // return view('chart.addchart');
     }
 
     /**
@@ -22,9 +25,94 @@ class chartController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function store(Request $request)
     {
-        //
+        // dd("YOOOOOOOOOOOOOOOO");
+        // return $request->input();
+        // ddd($request);
+        // echo($request);
+
+        // $request->validate([
+        //     'user_id' => 'required|string|min:3|max:255',
+        //     'chartName' => 'required|string|min:3|max:255',
+        //     'amountDataPoints' => 'required',
+        // ]);
+
+        // $query = DB::table('chart')->insert([
+        //     'user_id'->$request->input(Auth::user()->id),
+        //     'chart_name'->$request->input('chartName')
+        // ]);
+
+        // if($query){
+        //     return back()->with('success','Chart has been successfully added');
+        // }else{
+        //     return back()->with('fail','Something went wrong');
+        // }
+
+        $labels = array();
+        $data = array();
+        $colours = array();
+        $chartName = $request->input('chartName');
+        $chartType = $request->input('chartType');
+
+
+        for ($i = 0; $i < $request->input('amountDataPoints'); $i++) {
+            array_push($labels, $request->input("dataTextAreaD1" . strval($i)));
+            array_push($data , $request->input("dataTextAreaD2" . strval($i)));
+            // Returns a hex code for a 'truly random' color
+            array_push($colours, RandomColor::one(array(
+                'luminosity' => 'random',
+                'hue' => 'random',
+                'format' => 'rgbCss',
+            )));
+        }
+
+        // function generateChartJson(chartName, chartType, label, data, colours) {
+
+        $config = json_encode(array(
+            'type' => $chartType,
+            'data' => json_encode(array(
+                'labels' => $labels,
+                'datasets' => array(json_encode(array(
+                    'label' => $chartName,
+                    'data' => $data,
+                    'backgroundColor' => $colours,
+                    'hoverOffset' => 4,
+                ))))
+            ))
+        );
+
+        // {{-- const config = {
+        //     type: 'pie',
+        //     data:{
+        //         labels: [
+        //           'Red',
+        //           'Blue',
+        //           'Yellow'
+        //         ],
+        //         datasets: [{
+        //           label: 'My First Dataset',
+        //           data: [300, 50, 100],
+        //           backgroundColor: [
+        //             'rgb(255, 99, 132)',
+        //             'rgb(54, 162, 235)',
+        //             'rgb(255, 205, 86)'
+        //           ],
+        //           hoverOffset: 4
+        //         }]
+        //       };,
+        //   }; --}}
+
+        // }
+
+        $chart = new Chart();
+        $chart->user_id = Auth::user()->id;
+        $chart->chart_name = $chartName;
+        $chart->chart_type = $chartType;
+        $chart->config = $config;
+        $chart->save();
+        return redirect()->back()->with('status', 'Student Added Successfully');
+
     }
 
     /**
@@ -33,7 +121,7 @@ class chartController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function create(Request $request)
     {
         //
     }
@@ -42,7 +130,7 @@ class chartController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
+     * @return \Iluminate\Http\Response
      */
     public function show(User $user)
     {
