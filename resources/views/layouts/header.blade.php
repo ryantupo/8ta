b8ae1b6000f61b8532c249dd83ce0681b00f2b87
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-03-06 19:36:09 +0000

    addition of delete icon not working yet

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -29,8 +29,15 @@
 
                         <div id="myDropdown" class="dropdown-content">
 
+
                             @foreach (json_decode($usercharts, true) as $data)
-                                <a href="/chart/{{ $data['id'] }}">{{ $data['chart_name'] }}</a>
+                                <div style="display: flex; justify-content: space-between;">
+                                    <a href="/chart/{{ $data['id'] }}">{{ $data['chart_name'] }}</a>
+                                    <button style="" type="button" class="bi bi-trash btn">                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                      </svg></button>
+                                </div>
                             @endforeach
                         </div>
                     </div>
