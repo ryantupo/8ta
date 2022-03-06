85cf94f5d691504da71d266e093aa75d8ea815c0
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-03-06 16:47:49 +0000

    rename

--- app/Models/chart.php
+++ app/Models/chart.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class chart extends Model
+class Chart extends Model
 {
     use HasFactory;
     protected $table = 'charts';
