c195997e434b64277ea2ee3a8c5d429a7ef76b24
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-03-07 23:56:56 +0000

    icon changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,7 @@
 <body class="antialiased">
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/901630e084.js" crossorigin="anonymous"></script>
 
     <div class="flex justify-center pt-8 sm:justify-start sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <a href="/"><img src="{{ asset('images/8taLogoSmall.png') }}" class="w-30 p-6" alt="8talogo"
@@ -13,7 +14,11 @@
         aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" style="margin-left:5px;">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
+
+
+
+
+    <div style="float:left;" class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto" style="margin-left: 1.5rem">
             <li class="nav-item active">
                 <a class="nav-link" href="/">Home</a>
@@ -22,6 +27,9 @@
                 <a class="nav-link" href="/blog">Blog</a>
             </li>
             @auth
+                <li class="nav-item active">
+                    <a class="nav-link" href="/favourites">Favourites</a>
+                </li>
                 <li class="nav-item active dropdown">
                     <div class="dropdown">
                         <button onclick="myFunction()" class="nav-link dropbtn"
@@ -34,11 +42,15 @@
                                 <div style="display: flex; justify-content: space-between;">
                                     <a href="/chart/{{ $data['id'] }}">{{ $data['chart_name'] }}</a>
                                     <form method="POST" id="delete-form" action="/deletechart/{{ $data['id'] }}">
-                                        {{csrf_field()}}
-                                        <button style="margin-top:10px;" name="_method" value="post" type="submit" class="bi bi-trash btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                          </svg></button>
+                                        {{ csrf_field() }}
+                                        <button style="margin-top:10px;" name="_method" value="post" type="submit"
+                                            class="bi bi-trash btn"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                            </svg></button>
                                     </form>
                                 </div>
                             @endforeach
@@ -49,35 +61,36 @@
                 <li class="nav-item active">
                     <a class="nav-link" href="/addchart">+</a>
                 </li>
-                <li class="nav-item active">
-                @endauth
-
-                @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/home') }}"
-                                class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log
-                                in</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}"
-                                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-
-            </li>
+            @endauth
         </ul>
     </div>
-</nav>
+    <ul style="float:right;" class="navbar-nav mr-auto">
+        <li class="nav-item active">
 
+            @if (Route::has('login'))
+                <div class="top-0 right-0 px-6 py-4 ">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"><i
+                                class="fa-solid fa-user"></i></a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"><i
+                                class="fa-solid fa-right-to-bracket fa-shake"></i></a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"><i
+                                    class="fa-solid fa-user-plus"></i></a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </li>
+    </ul>
+</nav>
 
 <script>
     /* When the user clicks on the button,
-    toggle between hiding and showing the dropdown content */
+        toggle between hiding and showing the dropdown content */
     function myFunction() {
         document.getElementById("myDropdown").classList.toggle("show");
     }
@@ -98,6 +111,18 @@
 </script>
 
 <style>
+    .login-mobile {}
+
+    @media only screen and (min-width: 992px) {
+        .login-mobile {
+            display: none;
+             !important
+            /* remove the element from page and DOM. */
+            visibility: hidden;
+             !important;
+        }
+    }
+
     .dropbtn {
         color: white;
         padding: 16px;
