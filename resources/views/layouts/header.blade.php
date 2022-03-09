8e52c602296bc7eb0d8053c591565c07f9d46358
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-03-09 16:03:33 +0000

    additioonal of toggleable favourite icons

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -39,30 +39,58 @@
 
 
                             @foreach (json_decode($usercharts, true) as $data)
-                                <div style="display: flex; justify-content: space-between;">
-                                    <a href="/chart/{{ $data['id'] }}">{{ $data['chart_name'] }}</a>
-                                    <form method="POST" id="delete-form" action="/deletechart/{{ $data['id'] }}">
-                                        {{ csrf_field() }}
-                                        <button style="margin-top:10px;" name="_method" value="post" type="submit"
-                                            class="bi bi-trash btn"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                            </svg></button>
-                                    </form>
-                                </div>
+                                <div>
+                                    <div style="display: flex; justify-content: space-between;">
+                                        <a href="/chart/{{ $data['id'] }}">{{ $data['chart_name'] }}</a>
+                                        <form method="POST" id="delete-form" action="/deletechart/{{ $data['id'] }}">
+                                            {{ csrf_field() }}
+                                            <button style="margin-top:10px;" name="_method" value="post" type="submit"
+                                                class="bi bi-trash btn"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                    height="16" fill="currentColor" class="bi bi-trash"
+                                                    viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                    <path fill-rule="evenodd"
+                                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                </svg></button>
+                                        </form>
+
+
+
+                                        <div style="margin-top:10px;">
+                                            <form method="POST" id="favourite-form"
+                                                action="/favouriteChart/{{ $data['id'] }}">
+                                                {{ csrf_field() }}
+                                                <button style="margin:auto;padding-right:15px;" name="_methodFavourite" value="post"
+                                                    type="submit">
+
+                                                    @if ($data['if_favourite'] == 1)
+                                                        <span style="color: goldenrod;">
+
+                                                            <i color:yellow; class="fa-solid fa-star"></i>
+
+                                                        </span>
+
+                                                    @else
+
+                                                        <i color:yellow; class="fa-solid fa-star"></i>
+                                                    @endif
+                                                </button>
+                                            </form>
+
+                                        </div>
+                                    </div>
                             @endforeach
                         </div>
                     </div>
-                </li>
+        </div>
+        </li>
 
-                <li class="nav-item active">
-                    <a class="nav-link" href="/addchart">+</a>
-                </li>
-            @endauth
-        </ul>
+        <li class="nav-item active">
+            <a class="nav-link" href="/addchart">+</a>
+        </li>
+    @endauth
+    </ul>
     </div>
     <ul style="float:right;" class="navbar-nav mr-auto">
         <li class="nav-item active">
