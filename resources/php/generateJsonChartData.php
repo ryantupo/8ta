433e43dca76594f33828b770451e446c45206969
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-04-30 12:56:49 +0100

    script to generate test json charts

---
+++ resources/php/generateJsonChartData.php
@@ -0,0 +1,21 @@
+<?php
+
+function generateRandomString($length = 25) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$data = array();
+
+
+for($i = 0; $i<= 5; $i++){
+  $data[generateRandomString(5)] = rand(1,999);
+}
+
+
+print_r(json_encode($data));
