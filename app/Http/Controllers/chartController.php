a910b5752adb99b7e2ff0bc4b8dea976734ca959
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-05-28 22:41:31 +0100

    updated update functions

--- app/Http/Controllers/chartController.php
+++ app/Http/Controllers/chartController.php
@@ -34,6 +34,7 @@ class chartController extends Controller
         $chartName = $request->input('chartName');
         $chartType = $request->input('chartType');
         $chartJsonData = $request->input('chartJsonData');
+        $raw_data = $chartJsonData;
         $chartJsonData = json_decode($chartJsonData, true);
 
         try {
@@ -49,24 +50,11 @@ class chartController extends Controller
             }
 
         } catch (Exception $e) {
-            echo($e);
+            echo ($e);
         }
 
-        //depricated code
-
-        // for ($i = 0; $i < $request->input('amountDataPoints'); $i++) {
-        //     array_push($labels, $request->input("dataTextAreaD1" . strval($i)));
-        //     array_push($data, $request->input("dataTextAreaD2" . strval($i)));
-        //     // Returns a hex code for a 'truly random' color
-        //     array_push($colours, RandomColor::one(array(
-        //         'luminosity' => 'light',
-        //         'hue' => 'random',
-        //         'format' => 'rgbCss',
-        //     )));
-        // }
-
         if ($chartName == "") {
-            return back()->with('fail', 'Chart was not named');
+            return redirect()->back()->with('fail', 'Chart was not named');
         }
 
         if (count($labels) > 0 && count($data) > 0) {
@@ -93,55 +81,17 @@ class chartController extends Controller
             );
 
             $query = DB::table('charts')->insert(
-                ['user_id' => Auth::user()->id, 'chart_name' => $chartName, 'chart_type' => $chartType, 'config' => $config]
+                ['user_id' => Auth::user()->id, 'chart_name' => $chartName, 'chart_type' => $chartType, 'config' => $config, 'raw_data' => $raw_data]
             );
 
             if ($query) {
-                return back()->with('success', 'Chart has been successfully added');
+                return redirect()->back()->with('success', 'Chart has been successfully added');
             } else {
-                return back()->with('fail', 'Something went wrong');
+                return redirect()->back()->with('fail', 'Something went wrong');
             }
         } else {
-            return back()->with('fail', 'Data Entered incorrectly');
+            return redirect()->back()->with('fail', 'Data Entered incorrectly');
         }
-
-        //depricated code
-        // if (count($labels) == $request->input('amountDataPoints') && count($data) == $request->input('amountDataPoints')) {
-
-        //     // function generateChartJson(chartName, chartType, label, data, colours) {
-
-        //     $datasets = new stdClass();
-        //     $datasets->label = $chartName;
-        //     $datasets->data = $data;
-        //     $datasets->backgroundColor = $colours;
-        //     $datasets->hoverOffset = 4;
-
-        //     $datasetsArray = [];
-
-        //     array_push($datasetsArray, $datasets);
-
-        //     $data = new stdClass();
-        //     $data->labels = $labels;
-        //     $data->datasets = $datasetsArray;
-
-        //     $config = json_encode(array(
-        //         'type' => $chartType,
-        //         'data' => $data)
-        //     );
-
-        //     $query = DB::table('charts')->insert(
-        //         ['user_id' => Auth::user()->id, 'chart_name' => $chartName, 'chart_type' => $chartType, 'config' => $config]
-        //     );
-
-        //     if ($query) {
-        //         return back()->with('success', 'Chart has been successfully added');
-        //     } else {
-        //         return back()->with('fail', 'Something went wrong');
-        //     }
-        // } else {
-        //     dd("error happened");
-        //     return back()->with('fail', 'Data Entered incorrectly');
-        // }
 
     }
 
@@ -156,9 +106,9 @@ class chartController extends Controller
         $query = DB::table('charts')->where('user_id', '=', Auth::user()->id)->where('id', '=', $id)->delete();
 
         if ($query) {
-            return back()->with('success', 'Chart has been successfully deleted');
+            return redirect()->back()->with('success', 'Chart has been successfully deleted');
         } else {
-            return back()->with('fail', 'Something went wrong');
+            return redirect()->back()->with('fail', 'Something went wrong');
         }
     }
 
@@ -177,9 +127,9 @@ class chartController extends Controller
         // $query = DB::table('charts')->where('user_id', '=', Auth::user()->id)->where('id', $id)->update(array('if_favourite' => !$original_val));
 
         if ($query) {
-            return back()->with('success', 'Chart has been successfully deleted');
+            return redirect()->back()->with('success', 'Chart has been successfully Favourited');
         } else {
-            return back()->with('fail', 'Something went wrong');
+            return redirect()->back()->with('fail', 'Something went wrong');
         }
     }
 
@@ -225,7 +175,80 @@ class chartController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $labels = array();
+        $data = array();
+        $colours = array();
+
+        $chartName = $request->input('chartName');
+        $chartType = $request->input('chartType');
+        $chartJsonData = $request->input('chartJsonData');
+        $raw_data = $chartJsonData;
+        $chartJsonData = json_decode($chartJsonData, true);
+        $id = $request->id;
+
+
+        try {
+            foreach ($chartJsonData as $key => $value) {
+                array_push($labels, $key);
+                array_push($data, $value);
+                // Returns a hex code for a 'truly random' color
+                array_push($colours, RandomColor::one(array(
+                    'luminosity' => 'light',
+                    'hue' => 'random',
+                    'format' => 'rgbCss',
+                )));
+            }
+
+        } catch (Exception $e) {
+            echo ($e);
+        }
+
+        if ($chartName == "") {
+            return redirect()->back()->with('fail', 'Chart was not named');
+        }
+
+        if (count($labels) > 0 && count($data) > 0) {
+
+            // function generateChartJson(chartName, chartType, label, data, colours) {
+
+            $datasets = new stdClass();
+            $datasets->label = $chartName;
+            $datasets->data = $data;
+            $datasets->backgroundColor = $colours;
+            $datasets->hoverOffset = 4;
+
+            $datasetsArray = [];
+
+            array_push($datasetsArray, $datasets);
+
+            $data = new stdClass();
+            $data->labels = $labels;
+            $data->datasets = $datasetsArray;
+
+            $config = json_encode(array(
+                'type' => $chartType,
+                'data' => $data)
+            );
+
+            // DB::table('area')
+            //     ->where('id', $request->get('area_id'))
+            //     ->update([
+            //         'island_group_id' => $request->get('island_group_id'),
+            //         'region_id' => $request->get('region_id'),
+            //     ]);
+
+            $query = DB::table('charts')->where('id', $id)->update(['user_id' => Auth::user()->id, 'chart_name' => $chartName, 'chart_type' => $chartType, 'config' => $config, 'raw_data' => $raw_data]);
+
+            // print_r($query);
+
+            if ($query) {
+                return redirect()->back()->with('success', 'Chart has been successfully Updated');
+            } else {
+                return redirect()->back()->with('fail', 'Something went wrong');
+            }
+        } else {
+            return redirect()->back()->with('fail', 'Data Entered incorrectly');
+        }
     }
 
     /**
