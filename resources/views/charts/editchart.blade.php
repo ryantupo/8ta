ae591f5a5acb5c7e4b02b7ba79327a6550af9dbc
Ryan Tuplin <ryan.tuplin@bodis.com>
2022-05-28 22:43:41 +0100

    edit chart blade and formn

---
+++ resources/views/charts/editchart.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.layout')
+
+
+@section('content')
+
+<div id="mainForm" class="form">
+    <form action="/editchart/{{ $chart->id }}" method="POST">
+
+        @csrf
+        <div class="mt-3 form-group">
+            <label for="chartName">Chart Name</label>
+            <input type="text" class="form-control" id="chartName" placeholder="Enter Chart Name" value="{{ $chart->chart_name }}"
+                maxlength="18" name="chartName" required>
+        </div>
+
+        {{-- the type of chart --}}
+        <div class="mt-3 form-group">
+            <label for="charts">Type Of Chart</label>
+            <select class="form-control" id="chart" value="{{ $chart->chart_type }}" name="chartType">
+                <option>pie</option>
+                <option>line</option>
+            </select>
+        </div>
+
+
+        <div class="mt-3 form-group">
+            <p style="font-size: 12px;" for="chartJsonData">Example of formatter Json ->
+                {"YjKMR":271,"WYox6":939,"BG1VE":406,"2ULc1":917} <- </p>
+                    <label for="chartJsonData">chartJsonData</label>
+                    <input type="text" class="form-control" id="chartJsonData" placeholder="Enter Chart json data"
+                        value="{{ $chart->raw_data }}" name="chartJsonData" maxlength="850" required>
+        </div>
+
+        {{-- data names and values --}}
+        <div id="datasets" class="form-group"></div>
+
+        <button style="margin:auto;" type="submit" class="mt-3 button btn btn-success btn-lg center-block"
+            id="submitBtn">Update Chart</button>
+
+    </form>
+</div>
+
+<style>
+    .form {
+        margin: auto;
+        margin-top: 50px;
+        width: 50%;
+    }
+
+    .label {
+        display: inline-flex;
+        align-items: center;
+        cursor: pointer;
+        color: #394a56;
+    }
+
+    .label-text {
+        margin-left: 16px;
+    }
+
+    .toggle {
+        margin-left: 15px;
+        margin-right: 15px;
+        isolation: isolate;
+        position: relative;
+        height: 30px;
+        width: 100px;
+        border-radius: 15px;
+        overflow: hidden;
+        box-shadow:
+            -8px -4px 8px 0px #ffffff,
+            8px 4px 12px 0px #d1d9e6,
+            4px 4px 4px 0px #d1d9e6 inset,
+            -4px -4px 4px 0px #ffffff inset;
+    }
+
+    .toggle-state {
+        display: none;
+    }
+
+    .indicator {
+        height: 100%;
+        width: 200%;
+        background: #ecf0f3;
+        border-radius: 15px;
+        transform: translate3d(-75%, 0, 0);
+        transition: transform 0.4s cubic-bezier(0.85, 0.05, 0.18, 1.35);
+        box-shadow:
+            -8px -4px 8px 0px #ffffff,
+            8px 4px 12px 0px #d1d9e6;
+    }
+
+    .toggle-state:checked~.indicator {
+        transform: translate3d(25%, 0, 0);
+    }
+
+
+
+    #toggle:checked~.control-me {
+        display: none;
+        /*this works*/
+    }
+
+    #toggle:~.control-me {
+        visibility: visible;
+        /*this works*/
+    }
+
+    #toggle:checked~.value {
+        display: none;
+        /*this does not work*/
+    }
+
+</style>
+
+
+@endsection
